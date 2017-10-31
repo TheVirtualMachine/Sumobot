fb5a3abba9450c80608046280af80e37c3c8285f
TheVirtualMachine <wongtech@vincemacri.ca>
2017-10-30 22:51:19 -0400

    Motor stuff.

--- Sumobot.ino
+++ Sumobot.ino
@@ -1,7 +1,15 @@
-// Define pin numbers.
-#define TRIGGER 9 // The trigger pin for the ultrasonic sensor.
-#define ECHO 10 // The echo pin for the ultrasonic sensor.
+// Define sensor pin numbers.
+#define TRIGGER 3 // The trigger pin for the ultrasonic sensor.
+#define ECHO 4 // The echo pin for the ultrasonic sensor.
 #define LDR_PIN A0 // The analog pin for LDR input,
+
+// Define motor pin numbers.
+#define LEFT_ENABLE 5 // The pin to turn the left motor on and off.
+#define LEFT_FORWARD 6 // The pin to move the left motor forwards.
+#define LEFT_BACKWARD 7 // The pin to move the left motor forwards.
+#define RIGHT_ENABLE 8 // The pin to turn the left motor on and off.
+#define RIGHT_FORWARD 9 // The pin to move the left motor forwards.
+#define RIGHT_BACKWARD 10 // The pin to move the left motor forwards.
 
 // Define constants.
 #define SPEED_OF_SOUND 0.03434 // Speed of sound as STP in cm per microsecond.
@@ -11,8 +19,18 @@ long duration;
 double distance;
 
 void setup() {
+  // Setup the ultrasonic sensor.
   pinMode(TRIGGER, OUTPUT); // Set the trigger pin as an output.
   pinMode(ECHO, INPUT); // Set the echo pin as an input.
+
+  // Setup the motors.
+  pinMode(LEFT_ENABLE, OUTPUT);
+  pinMode(LEFT_FORWARD, OUTPUT);
+  pinMode(LEFT_BACKWARD, OUTPUT);
+  pinMode(RIGHT_ENABLE, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT);
+  pinMode(RIGHT_BACKWARD, OUTPUT);
+  
   Serial.begin(9600); // Start the serial communication.
 }
 
@@ -45,7 +63,33 @@ void readUltrasonic() {
   delay(75);
 }
 
+void dance() {
+	// Enable motors.
+	digitalWrite(LEFT_ENABLE, HIGH);
+	digitalWrite(RIGHT_ENABLE, HIGH);
+
+	// Move forward.
+	digitalWrite(LEFT_FORWARD, HIGH);
+	digitalWrite(LEFT_BACKWARD, LOW);
+	digitalWrite(RIGHT_FORWARD, HIGH);
+	digitalWrite(RIGHT_BACKWARD, LOW);
+	delay(1000);
+
+	// Turn around.
+	digitalWrite(LEFT_FORWARD, HIGH);
+	digitalWrite(LEFT_BACKWARD, LOW);
+	digitalWrite(RIGHT_FORWARD, LOW);
+	digitalWrite(RIGHT_BACKWARD, HIGH);
+	delay(1000);
+
+	// Disable motors.
+	digitalWrite(LEFT_ENABLE, LOW);
+	digitalWrite(RIGHT_ENABLE, LOW);
+	delay(3000);
+}
+
 void loop() {
   readLDR();
   readUltrasonic();
+  dance();
 }
